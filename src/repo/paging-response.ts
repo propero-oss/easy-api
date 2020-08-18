d494c12102f1c8b5b4c0729ac9e3ef5e5e1e83c7
Merlin <merlin.reichwald@gmail.com>
2020-08-18 18:17:06 +0200

    fix: remove unused repo service

--- src/repo/paging-response.ts
+++
@@ -1,6 +0,0 @@
-export interface PagingResponse<T> {
-  top: number;
-  skip: number;
-  total: number;
-  data: T[];
-}
--- src/util/array-wrap.ts
+++ src/util/array-wrap.ts
@@ -1,3 +1,3 @@
 export function arrayWrap<T>(obj: T): T extends unknown[] ? T : T[] {
-  return Array.isArray(obj) ? obj : [obj] as any;
+  return Array.isArray(obj) ? obj : ([obj] as any);
 }
