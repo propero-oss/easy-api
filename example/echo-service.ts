e2f98ab8b22d41295c8d86740d5a87273f7b188c
Merlin <merlin.reichwald@gmail.com>
2020-08-25 13:27:56 +0200

    fix: parameter injector meta

--- example/echo-service.ts
+++ example/echo-service.ts
@@ -1,11 +1,11 @@
 import { Mount } from "example/app-init";
 import { Request } from "express";
-import { Use, Service, createService } from "src/index";
+import { Use, Service } from "src/index";
 
-@Mount()
+@Mount("/echo")
 @Service()
 class EchoService {
-  @Use("/echo")
+  @Use()
   public async echo(req: Request) {
     const { query, body, path, originalUrl: url, baseUrl, protocol, cookies, ip, hostname, headers } = req;
     return { path, url, baseUrl, protocol, cookies, ip, hostname, headers, query, body };
