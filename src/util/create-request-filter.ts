40a88c2c978a110fddc627e942abb8201314fe50
Merlin <merlin.reichwald@gmail.com>
2020-09-09 13:40:54 +0200

    fix: allow for registration of custom request option filters

--- src/util/create-request-filter.ts
+++ src/util/create-request-filter.ts
@@ -1,20 +1,44 @@
 import { Request } from "express";
 import type { IncomingHttpHeaders } from "http";
-import { ErrorHandlerOptions, HttpHandlerOptions, RequestFilter } from "src/types";
+import { ErrorHandlerOptions, HttpHandlerOptions, MaybeArray, RequestFilter } from "src/types";
 import { arrayWrap } from "src/util/array-wrap";
+
+export const filterRegistry: { [K in keyof ErrorHandlerOptions]: ((value: Required<ErrorHandlerOptions>[K]) => RequestFilter)[] } = {};
+
+export function registerFilter<K extends keyof ErrorHandlerOptions>(
+  option: K,
+  filterGenerator: (value: Required<ErrorHandlerOptions>[K]) => RequestFilter
+): void {
+  // Typescript please the type of the expression before .push should be
+  // ((value: Required<ErrorHandlerOptions>[K]) => RequestFilter)[]
+  // and pushing filterGenerator should not be an issue because it matches this
+  // signature but somehow an any[] cast is required to make this work...
+  ((filterRegistry[option] ?? (filterRegistry[option] = [])) as any[]).push(filterGenerator);
+}
 
 export const needsRequestFilter = (options: HttpHandlerOptions): boolean => Object.keys(options).length > 0;
 
 export function buildFilters(options: HttpHandlerOptions | ErrorHandlerOptions, method: string, errorHandler: boolean): RequestFilter[] {
   const checks: RequestFilter[] = [];
-  const { contentType, accept, headers, classes } = options as ErrorHandlerOptions;
+  const { classes } = options as ErrorHandlerOptions;
 
-  if (contentType) checks.push(filterContentType(contentType));
-  if (accept) checks.push(filterAccept(accept));
-  if (headers) checks.push(filterHeaders(headers));
+  for (const key of Object.keys(options))
+    if (filterRegistry[key as keyof ErrorHandlerOptions])
+      // Typing is solid but typing of Object.keys to not return keyof T but string[] is causing problems
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      checks.push(...filterRegistry[key as keyof ErrorHandlerOptions]!.map((it) => it(options[key])));
+
   if (errorHandler && classes) checks.push(filterClasses(classes));
 
   return checks;
+}
+
+export function stringOrRegExpMatch(original = "", options: MaybeArray<string | RegExp>): boolean {
+  const lower = original.toLowerCase();
+  return !!arrayWrap(options).find((option) =>
+    typeof option === "string" ? lower.startsWith(option.toLowerCase()) : option.test(original)
+  );
 }
 
 export function createRequestFilter(
@@ -26,22 +50,29 @@ export function createRequestFilter(
   return (req: Request): boolean => !checks.find((it) => !it(req));
 }
 
-export const filterContentType = (contentType: string | string[]): RequestFilter => (req) =>
-  arrayWrap(contentType).includes(req.headers["content-type"] ?? "");
+registerFilter(
+  "contentType",
+  (contentType: MaybeArray<string | RegExp>): RequestFilter => (req) => stringOrRegExpMatch(req.headers["content-type"], contentType)
+);
 
-export const filterAccept = (accepts: string | string[]): RequestFilter => (req) => {
-  const value = req.accepts();
-  for (const accept of arrayWrap(accepts)) if (value.includes(accept)) return true;
-  return false;
-};
+registerFilter(
+  "accept",
+  (accepts: MaybeArray<string | RegExp>): RequestFilter => (req) => !!req.accepts().find((it) => stringOrRegExpMatch(it, accepts))
+);
 
-export const filterHeaders = (headers: IncomingHttpHeaders): RequestFilter => (req) => {
-  for (const [header, value] of Object.entries(headers)) {
-    const reqHeader = arrayWrap(req.headers[header]!);
-    const headers = arrayWrap(value!);
-    for (const header of headers) if (!reqHeader.includes(header)) return false;
+registerFilter(
+  "headers",
+  (headers: IncomingHttpHeaders): RequestFilter => (req) => {
+    for (const [header, value] of Object.entries(headers)) {
+      const reqHeader = arrayWrap(req.headers[header]!);
+      const headers = arrayWrap(value!);
+      for (const header of headers) if (!reqHeader.includes(header)) return false;
+    }
+    return true;
   }
-  return true;
-};
+);
 
-export const filterClasses = (classes: unknown[]): RequestFilter => (req) => !!classes.find((cls) => req.__error instanceof (cls as any));
+registerFilter("method", (method): RequestFilter => (req) => stringOrRegExpMatch(req.method, method));
+
+export const filterClasses = (classes: MaybeArray<unknown>): RequestFilter => (req) =>
+  !!arrayWrap(classes).find((cls) => req.__error instanceof (cls as any));
