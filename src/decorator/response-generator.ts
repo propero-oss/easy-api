74472786b6b53b33e3e762f1c84fcbd536c1371d
Merlin <merlin.reichwald@gmail.com>
2020-09-09 13:42:50 +0200

    feat: add stream and auto response generators

--- src/decorator/response-generator.ts
+++ src/decorator/response-generator.ts
@@ -5,6 +5,26 @@ export function registerResponseType(responseType: string, generator: ResponseGe
   responseTypes[responseType as ExpressResponseType] = generator;
 }
 
+registerResponseType("auto", (middleware, status) => async (req, res, next) => {
+  const result = await middleware(req, res, next);
+  if (status) res.status(status);
+  if (result == null) return res.send();
+  if (typeof result === "string") return res.send(result);
+  if (typeof result === "object" && result && "pipe" in result && typeof (result as any).pipe === "function") {
+    // Assume stream
+    (result as any).on("end", () => res.end());
+    (result as any).pipe(res);
+  }
+  res.json(result);
+});
+
+registerResponseType("stream", (middleware, status) => async (req, res, next) => {
+  const result: any = await middleware(req, res, next);
+  if (status) res.status(status);
+  result.on("enc", () => res.end());
+  result.pipe(res);
+});
+
 registerResponseType("json", (middleware, status) => async (req, res, next) => {
   const result = await middleware(req, res, next);
   if (status) res.status(status);
