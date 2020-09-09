a7d18f019a02b2c726c4f6e18eb191e37a1615c1
Merlin <merlin.reichwald@gmail.com>
2020-09-09 13:38:11 +0200

    chore: update examples to include status, and content type filters

--- example/echo-service.ts
+++ example/echo-service.ts
@@ -1,12 +1,19 @@
 import { Mount } from "example/app-init";
 import { Request } from "express";
-import { Use, Service } from "src/index";
+import { All, Service } from "src/index";
 
 @Mount("/echo")
 @Service()
 class EchoService {
-  @Use()
-  public async echo(req: Request) {
+  @All("*", { accept: "text/html", responseType: "raw" })
+  public async echoHtml(req: Request) {
+    const { query, body, path, originalUrl: url, baseUrl, protocol, cookies, ip, hostname, headers } = req;
+    return `<pre>${JSON.stringify({ path, url, baseUrl, protocol, cookies, ip, hostname, headers, query, body }, undefined, 2)}</pre>`;
+  }
+
+  @All("*", { accept: "application/json", responseType: "json" })
+  public async echoJson(req: Request) {
+    console.log("WHY AM I CALLED??????????");
     const { query, body, path, originalUrl: url, baseUrl, protocol, cookies, ip, hostname, headers } = req;
     return { path, url, baseUrl, protocol, cookies, ip, hostname, headers, query, body };
   }
