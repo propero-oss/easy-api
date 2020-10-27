034960f4ad37900ae2e8e30094313c46d67bf2f9
Merlin Reichwald <merlin.reichwald@gmail.com>
2020-10-27 20:33:55 +0100

    test: add unit tests for metadata functions

---
+++ test/meta/parameter-meta.test.ts
@@ -0,0 +1,32 @@
+import { createParameterMetaAccessors, createParameterMetaDecoratorFactory } from "src/meta";
+
+describe("meta/parameter-meta.ts", () => {
+  const meta = { foo: "bar" };
+  describe("createParameterMetaAccessors(name)", () => {
+    const { getMeta, setMeta } = createParameterMetaAccessors<any>();
+    it("should attach metadata to a given parameter", () => {
+      // eslint-disable-next-line
+      class Test { foo() {} }
+      setMeta(Test, "foo", 0, meta);
+      expect(getMeta(Test, "foo")).toEqual({ 0: meta });
+    });
+
+    it("should overwrite existing parameter metadata", () => {
+      // eslint-disable-next-line
+      class Test { foo() {} }
+      setMeta(Test, "foo", 0, { bar: "baz" });
+      setMeta(Test, "foo", 0, meta);
+      expect(getMeta(Test, "foo")).toEqual({ 0: meta });
+    });
+  });
+  describe("createParameterMetaDecoratorFactory(setMeta)(generator)", () => {
+    const { getMeta, setMeta } = createParameterMetaAccessors<any>();
+    const factory = createParameterMetaDecoratorFactory<any>(setMeta);
+    it("should create a parameter decorator for a given generator", () => {
+      const ParamDecorator = factory(() => meta);
+      // eslint-disable-next-line
+      class Test { foo(@ParamDecorator param: any) {} }
+      expect(getMeta(Test, "foo")).toEqual({ 0: meta });
+    });
+  });
+});
---
+++ test/meta/router-meta.test.ts
@@ -0,0 +1,32 @@
+import { addRouterMeta, getRouterMeta } from "src/meta";
+import { RouterMeta } from "src/types";
+
+describe("meta/router-meta.ts", () => {
+  const meta: RouterMeta = { path: "/", factory: (instance) => undefined as any };
+  describe("addRouterMeta(cls, meta)", () => {
+    it("should add metadata to a given class", () => {
+      class Test {}
+      addRouterMeta(Test, meta);
+      expect(getRouterMeta(Test)).toEqual(meta);
+    });
+  });
+  describe("getRouterMeta(cls)", () => {
+    class Parent {}
+    class Child1 extends Parent {}
+    class Child2 extends Child1 {}
+    class Child3 extends Child2 {}
+    addRouterMeta(Parent, meta);
+
+    it("should get metadata for a given class", () => {
+      expect(getRouterMeta(Parent)).toEqual(meta);
+    });
+
+    it("should get metadata from a given classes prototype chain", () => {
+      expect(getRouterMeta(Child3)).toEqual(meta);
+    });
+
+    it("should throw for a class without router meta", () => {
+      expect(() => getRouterMeta(class {})).toThrow();
+    });
+  });
+});
